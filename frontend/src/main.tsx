a63c80b72809d98b02e1c4220411efc7db2294ee
Diego Merello <diego@wipitalia.it>
2025-06-01 00:06:52 +0200

    songs route

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home.tsx'
 import Register from './pages/Register.tsx'
+import Songs from './pages/Songs.tsx'
 import './index.css'
 import './base-layout.css'
 
@@ -12,6 +13,7 @@ createRoot(document.getElementById('root')!).render(
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/songs" element={<Songs />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
---
+++ frontend/src/pages/Songs.tsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+
+function Songs() {
+
+  return (
+    <>
+      <div className="flex gap-6 min-h-svh w-full items-center justify-center">
+        Songs page
+      </div>
+    </>
+  );
+}
+
+export default Songs;
